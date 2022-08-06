15a870f1b6342ce5d11ce8e7cbcd65a8de9f615c
lee7goal <909057156@qq.com>
2022-08-06 14:30:04 +0800

    新建应用

--- bili_auto_work/script.py
+++ bili_auto_work/script.py
@@ -1,4 +1,4 @@
-from bilibili_api import Credential, rank
+from bilibili_api import Credential, rank, video
 import asyncio
 
 SESSDATA = '64660c21%2C1675277136%2C7eb29%2A81'
@@ -7,6 +7,16 @@ BU_VID3 = 'EAC791BC-66CB-7DB7-F62D-2A2571EB1A1650950infoc'
 DEDE_USERID = '294718686'
 
 credential = Credential(sessdata=SESSDATA, bili_jct=BILI_JCT, buvid3=BU_VID3, dedeuserid=DEDE_USERID)
+async def video_info(bid):
+    v = video.Video(bvid=bid, credential=credential)
+    info = await v.get_info()
+    down_load_url = await v.get_download_url(page_index=0)
+
+    print(down_load_url)
 
 if __name__ == '__main__':
-    print(asyncio.get_event_loop().run_until_complete(rank.get_hot_videos()))
+    hot_video = asyncio.get_event_loop().run_until_complete(rank.get_hot_videos())
+    for single_video in hot_video['list']:
+        print(single_video['cid'])
+        asyncio.get_event_loop().run_until_complete(video_info(single_video['bvid']))
+
